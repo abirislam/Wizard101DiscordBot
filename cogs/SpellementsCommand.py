2bffa169b7e1e4def73b8876c418d9300a7d3e3b
abir itsmeee <akumareign@gmail.com>
2023-12-18 14:52:56 -0500

    spellements update

--- cogs/SpellementsCommand.py
+++ cogs/SpellementsCommand.py
@@ -12,8 +12,8 @@ class SpellementsCommand(commands.Cog):
     @commands.command()
     async def spellements(self, ctx):
         spellementsEmbed = discord.Embed(title="Blade's Spellement Farming Guide", color=discord.Color.random())
-        spellementsEmbed.add_field(name="", value=
-        "This embed is WIP, check back later!")
+        spellementsEmbed.add_field(name="This is the best way to farm spellements as of December 2023", value=
+        "Rank 1 Spellements - Make new characters and finish WC, you'll receive 75 rank 1 spellements from Abner K. Doodle\n\nNote: For rank 2-6 spellements you MUST not have completed Celestia on your character\nRank 2-6 Elemental & Balance Spellements - Boris BlackRock in the Crucible, Dragonspyre\nRank 2-6 Spiritual & Balance Spellements - Vasek AshWeaver in Vault 1933, Grand Chasm, Dragonspyre\n\nLoremaster spells now drop at the following gold key bosses:\nTakanobu the Masterless in Cave of Solitude, Mooshu\nDrowned Dan in Science Center, Celestia\nLambent Fire in Crystal Caves, Avalon\nBaron Von Bracken in Tanglewood Way, Wysteria\nIxcax CursedWing in Three Points, Azteca\nHigh Loremagus in the Atheneum, Dragonspyre\nLady Stonegaze in Garden of Hesperides, Aquila\nKing Borr in Savarstaad Pass, Grizzleheim\nSpirit of Ignorance, Crab Alley\n\nKaramelle, Lemuria, Novus, and Wallaru spellements I will add later")
 
         # file = discord.File("./images/spellements.png", filename="spellements.png")
         # spellementsEmbed.set_image(url="attachment://spellements.png")
